8de2567972e747e1260b1b55ea599c6943931e62
RangerCD <compact-disk@live.com>
2020-09-16 16:28:25 +0800

    add golang initialization

---
+++ init-golang.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source default.conf
+source workspace.conf
+
+# Install gvm
+$TARGET_SHELL < <(curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer)
+source ~/.gvm/scripts/gvm
+
+# Install golang bootstrap toolchain
+gvm install go1.4 -B
+
+# Compile install target golang
+gvm use go1.4
+gvm install go${TARGET_GOLANG_VERSION}
+
+# Config default golang environment
+gvm use go${TARGET_GOLANG_VERSION} --default
